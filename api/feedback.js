c3b4560ccf1db2ee49d21f5a44139f30ec8b4a90
yeheumyvettelah <yeheumyvette.lah@gmail.com>
2026-05-19 15:04:18 +0900

    Update feedback.js

--- api/feedback.js
+++ api/feedback.js
@@ -5,7 +5,7 @@
 
 const { SYSTEM_PROMPT } = require('./prompt');
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
